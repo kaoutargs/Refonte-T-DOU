7e3aba84ebc0d24bf116185319b2a7bd99ac8d1c
Kaoutar G'SSIR <kaoutargs@gmail.com>
2020-06-24 08:49:34 +0200

    initialisation du projet

---
+++ footer.php
@@ -0,0 +1,101 @@
+<footer>
+  <div class="container-fluid">
+    <div class="row">
+<div class="col-lg-12">
+  <div class="letter">
+    <p class="formtext">Lettre d’informations</p>
+<div class="mail">
+  <input class="text-center text-white" type="email" id="email" name="name" required placeholder="Votre e-mail"
+           minlength="4" maxlength="12" size="35">
+<a href="#!"><img class="telegram" alt="icon-telegram" src="img/telegram.png"></a>
+    </div>
+    </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Footer -->
+<div class="container-fluid link block">
+
+<!-- Grid row -->
+<div class="row mt-3">
+
+<div class="py-4 col-md-3 col-lg-4 col-xl-2 mx-auto">
+  <div class="logo-fb"><img class="logo-fb" href="#!" alt="icon-facebook" src="img/facebook.png"></div>
+
+   <h6 class="test  mt-0 d-inline-block mx-auto mb-4">Catégories</h6>
+     <p>
+       <a href="#!">Collection 1</a></p>
+     <p>
+       <a href="#!">Collection 2</a></p>
+     <p>
+       <a href="#!">Collection 3</a></p>
+  </div>
+
+<div class="py-4 col-md-2 col-lg-2 col-xl-2 mx-auto">
+
+ <!-- Links -->
+ <h6 class="test  mt-0 d-inline-block mx-auto mb-4">Informations</h6>
+   <p>
+     <a href="#!">Promotions</a></p>
+   <p>
+     <a href="#!">Nouveaux produits</a></p>
+   <p>
+     <a href="#!">Meilleures ventes</a></p>
+   <p>
+     <a href="#!">Contactez-nous</a></p>
+   <p>
+     <a href="#!">Conditions d’utilisations</a></p>
+   <p>
+     <a href="#!">CGV</a></p>
+   <p>
+     <a href="#!">Sitemap</a></p>
+  </div>
+
+<div class="py-4 col-md-3 col-lg-2 col-xl-2 mx-auto ">
+
+ <!-- Links -->
+ <h6 class="test mt-0 d-inline-block mx-auto mb-4">Mon compte</h6>
+
+   <a href="#!">Mes commandes</a></p>
+ <p>
+   <a href="#!">Mes avoirs</a></p>
+ <p>
+   <a href="#!">Mes adresses</a></p>
+ <p>
+   <a href="#!">Mes informations personnelles</a></p>
+
+</div>
+
+<!-- Mobile -->
+
+<div class="py-4 col-md-3 col-lg-2 col-xl-2 mx-auto ">
+
+<div class="infobout">
+  <h6 class="test mt-0 d-inline-block mx-auto">Informations sur votre boutique</h6>
+</div>
+</div>
+
+<div align="right"class="py-4 col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0">
+
+  <!-- Links -->
+<div class="coordonnees"><h6 class="py-4 mt-0 d-inline-block mx-auto">Coordonnées</h6>
+
+  <p>
+  TDOU ? RECONNU PARTOUT EN FRANCE ! <img class="icon" alt="icon-pin" src="img/pin.png"><a href="#!"> 17 Rue Paul Corteville 59290 Wasquehal, France</p></a>
+
+   <p>
+     Appelez-nous au :<a href="#!"> 06 95 07 48 65 <img class="icon" alt="icon-phone" src="img/phone.png"></p></a>
+   <p>
+      E-mail :<a href="#!"> contact@tdou.fr <img class="icon" alt="icon-mail" src="img/mail.png"></p></a>
+   <a href="#!"><p>
+    Nous suivre <img class="icon" alt="icon-facebook" src="img/facebook.png"></p></a>
+  </div>
+   </div>
+    </div>
+     </div>
+
+<!-- Copyright -->
+<div class="copyright text-center"><p class="copyright-text">©2020 - Logiciel e-commerce par PrestaShop™</p></div>
+
+</footer>
