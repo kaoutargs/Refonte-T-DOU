dd028edf978c33afe40d69fc52540dd7c1d15b28
Kaoutar G'SSIR <kaoutargs@gmail.com>
2020-06-25 10:54:26 +0200

    changements

--- footer.php
+++ footer.php
@@ -18,78 +18,106 @@
 <div class="container-fluid link block">
 
 <!-- Grid row -->
-<div class="row mt-3">
+<div class="row mt-4">
 
-<div class="py-4 col-md-3 col-lg-4 col-xl-2 mx-auto">
-  <div class="logo-fb"><img class="logo-fb" href="#!" alt="icon-facebook" src="img/facebook.png"></div>
+<div class=" col-md-2 col-lg-4 col-xl-2 mx-auto">
 
-   <h6 class="test  mt-0 d-inline-block mx-auto mb-4">Catégories</h6>
-     <p>
-       <a href="#!">Collection 1</a></p>
-     <p>
-       <a href="#!">Collection 2</a></p>
-     <p>
-       <a href="#!">Collection 3</a></p>
+  <div class="logo-fb "><img class="logo-fb" href="#!" alt="icon-facebook" src="img/facebook.png"></div>
+
+   <h6 class="linkfoot plus d-inline-block">Catégories</h6>
+
+   <ul class="list-unstyled">
+             <li>
+               <a href="#!">Collection 1</a>
+             </li>
+             <li>
+               <a href="#!">Collection 2</a>
+             </li>
+             <li>
+               <a href="#!">Collection 3</a>
+             </li>
+           </ul>
   </div>
 
-<div class="py-4 col-md-2 col-lg-2 col-xl-2 mx-auto">
+<div class=" col-md-2 col-lg-2 col-xl-2 mx-auto">
 
  <!-- Links -->
- <h6 class="test  mt-0 d-inline-block mx-auto mb-4">Informations</h6>
-   <p>
-     <a href="#!">Promotions</a></p>
-   <p>
-     <a href="#!">Nouveaux produits</a></p>
-   <p>
-     <a href="#!">Meilleures ventes</a></p>
-   <p>
-     <a href="#!">Contactez-nous</a></p>
-   <p>
-     <a href="#!">Conditions d’utilisations</a></p>
-   <p>
-     <a href="#!">CGV</a></p>
-   <p>
-     <a href="#!">Sitemap</a></p>
+ <h6 class="linkfoot plus mt-0 d-inline-block mx-auto mb-4">Informations</h6>
+
+ <ul class="list-unstyled">
+           <li>
+             <a href="#!">Promotions</a>
+           </li>
+           <li>
+             <a href="#!">Nouveaux produits</a>
+           </li>
+           <li>
+             <a href="#!">Meilleures ventes</a>
+           </li>
+           <li>
+             <a href="#!">Contactez-nous</a>
+           </li>
+           <li>
+             <a href="#!">Conditions d’utilisations</a>
+           </li>
+           <li>
+             <a href="#!">CGV</a>
+           </li>
+           <li>
+             <a href="#!">Sitemap</a>
+           </li>
+         </ul>
   </div>
 
-<div class="py-4 col-md-3 col-lg-2 col-xl-2 mx-auto ">
+<div class="py-3 col-md-3 col-lg-2 col-xl-2 mx-auto">
 
  <!-- Links -->
- <h6 class="test mt-0 d-inline-block mx-auto mb-4">Mon compte</h6>
+ <h6 class="linkfoot plus mt-0 d-inline-block mx-auto mb-4">Mon compte</h6>
 
-   <a href="#!">Mes commandes</a></p>
- <p>
-   <a href="#!">Mes avoirs</a></p>
- <p>
-   <a href="#!">Mes adresses</a></p>
- <p>
-   <a href="#!">Mes informations personnelles</a></p>
-
+ <ul class="list-unstyled">
+           <li>
+             <a href="#!">Mes commandes</a>
+           </li>
+           <li>
+             <a href="#!">Mes avoirs</a>
+           </li>
+           <li>
+             <a href="#!">Mes adresses</a>
+           </li>
+           <li>
+             <a href="#!">Mes informations personnelles</a>
+           </li>
+         </ul>
 </div>
 
 <!-- Mobile -->
 
-<div class="py-4 col-md-3 col-lg-2 col-xl-2 mx-auto ">
+<div class="infobout py-3 col-md-3 col-lg-2 col-xl-2 mx-auto">
 
 <div class="infobout">
-  <h6 class="test mt-0 d-inline-block mx-auto">Informations sur votre boutique</h6>
+  <h6 class="linkfoot plus">Informations sur votre boutique</h6>
 </div>
 </div>
 
-<div align="right"class="py-4 col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0">
+<div align="right"class="py-3 col-md-4 col-lg-3 col-xl-4 mx-auto mb-md-0">
 
   <!-- Links -->
 <div class="coordonnees"><h6 class="py-4 mt-0 d-inline-block mx-auto">Coordonnées</h6>
 
-  <p>
-  TDOU ? RECONNU PARTOUT EN FRANCE ! <img class="icon" alt="icon-pin" src="img/pin.png"><a href="#!"> 17 Rue Paul Corteville 59290 Wasquehal, France</p></a>
-
-   <p>
-     Appelez-nous au :<a href="#!"> 06 95 07 48 65 <img class="icon" alt="icon-phone" src="img/phone.png"></p></a>
-   <p>
-      E-mail :<a href="#!"> contact@tdou.fr <img class="icon" alt="icon-mail" src="img/mail.png"></p></a>
-   <a href="#!"><p>
-    Nous suivre <img class="icon" alt="icon-facebook" src="img/facebook.png"></p></a>
+  <ul class="list-unstyled">
+            <li>
+              <a href="#!">TDOU ? RECONNU PARTOUT EN FRANCE !<img class="icon" alt="icon-pin" src="img/pin.png"><a href="#!"> 17 Rue Paul Corteville 59290 Wasquehal, France</a>
+            </li>
+            <li>
+              <a>Appelez-nous au :<a href="#!"> 06 95 07 48 65 <img class="icon" alt="icon-phone" src="img/phone.png"></a></a>
+            </li>
+            <li>
+              <a>E-mail :<a href="#!"> contact@tdou.fr <img class="icon" alt="icon-mail" src="img/mail.png"></a></a>
+            </li>
+            <li>
+              <a href="#!">Nous suivre <img class="icon" alt="icon-facebook" src="img/facebook.png"></a>
+            </li>
+          </ul>
   </div>
    </div>
     </div>
