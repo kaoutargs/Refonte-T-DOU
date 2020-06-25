71dbd6c74d2edd15aca195827270aba774db2a03
Charles0899 <charles0899@live.fr>
2020-06-25 11:55:48 +0200

    images-charles

---
+++ img0/footer.php
@@ -0,0 +1,109 @@
+<footer>
+ <div class="container-fluid">
+ <div class="row">
+<div class="col-lg-12">
+ <div class="letter">
+ <p class="formtext">Lettre d’informations</p>
+<div class="mail">
+ <input class="text-center text-white" type="email" id="email" name="name" required placeholder="Votre e-mail"
+ minlength="4" maxlength="12" size="35">
+<a href="#!"><img class="telegram" alt="icon-telegram" src="img/telegram.png"></a>
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+ <!-- Footer -->
+<div class="container-fluid link block">
+<!-- Grid row -->
+<div class="row mt-4">
+<div class=" col-md-2 col-lg-4 col-xl-2 mx-auto">
+ <div class="logo-fb "><img class="logo-fb" href="#!" alt="icon-facebook" src="img/facebook.png"></div>
+ <h6 class="linkfoot plus d-inline-block ">Catégories</h6>
+ <ul class="list-unstyled">
+ <li>
+ <a href="#!">Collection 1</a>
+ </li>
+ <li>
+ <a href="#!">Collection 2</a>
+ </li>
+ <li>
+ <a href="#!">Collection 3</a>
+ </li>
+ </ul>
+ </div>
+<div class=" col-md-2 col-lg-2 col-xl-2 mx-auto">
+ <!-- Links -->
+ <h6 class="linkfoot plus mt-0 d-inline-block mx-auto mb-4">Informations</h6>
+ <ul class="list-unstyled">
+ <li>
+ <a href="#!">Promotions</a>
+ </li>
+ <li>
+ <a href="#!">Nouveaux produits</a>
+ </li>
+ <li>
+ <a href="#!">Meilleures ventes</a>
+ </li>
+ <li>
+ <a href="#!">Contactez-nous</a>
+ </li>
+ <li>
+ <a href="#!">Conditions d’utilisations</a>
+ </li>
+ <li>
+ <a href="#!">CGV</a>
+ </li>
+ <li>
+ <a href="#!">Sitemap</a>
+ </li>
+ </ul>
+ </div>
+<div class="py-3 col-md-3 col-lg-2 col-xl-2 mx-auto">
+ <!-- Links -->
+ <h6 class="linkfoot plus mt-0 d-inline-block mx-auto mb-4">Mon compte</h6>
+ <ul class="list-unstyled">
+ <li>
+ <a href="#!">Mes commandes</a>
+ </li>
+ <li>
+ <a href="#!">Mes avoirs</a>
+ </li>
+ <li>
+ <a href="#!">Mes adresses</a>
+ </li>
+ <li>
+ <a href="#!">Mes informations personnelles</a>
+ </li>
+ </ul>
+</div>
+<!-- Mobile -->
+<div class="infobout py-3 col-md-3 col-lg-2 col-xl-2 mx-auto">
+<div class="infobout">
+ <h6 class="linkfoot plus">Informations sur votre boutique</h6>
+</div>
+</div>
+<div align="right"class="py-3 col-md-4 col-lg-3 col-xl-4 mx-auto mb-md-0">
+ <!-- Links -->
+<div class="coordonnees"><h6 class="py-4 mt-0 d-inline-block mx-auto">Coordonnées</h6>
+ <ul class="list-unstyled">
+ <li>
+ <a href="#!">TDOU ? RECONNU PARTOUT EN FRANCE !<img class="icon" alt="icon-pin" src="pin.png"><a href="#!"> 17 Rue Paul Corteville 59290 Wasquehal, France</a>
+ </li>
+ <li>
+ <a>Appelez-nous au :<a href="#!"> 06 95 07 48 65 <img class="icon" alt="icon-phone" src="phone.png"></a></a>
+ </li>
+ <li>
+ <a>E-mail :<a href="#!"> contact@tdou.fr <img class="icon" alt="icon-mail" src="mail.png"></a></a>
+ </li>
+ <li>
+ <a href="#!">Nous suivre <img class="icon" alt="icon-facebook" src="facebook.png"></a>
+ </li>
+ </ul>
+ </div>
+ </div>
+ </div>
+ </div>
+<!-- Copyright -->
+<div class="copyright text-center"><p class="copyright-text">©2020 - Logiciel e-commerce par PrestaShop™</p></div>
+</footer>
---
+++ img0/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <title>TDOU</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;400;500;900&display=swap" rel="stylesheet">
+ <link rel="stylesheet" href="main.css">
+ <link rel="icon" type="image/png" href="favicon">
+ <link rel="shortcut icon" type="image/x-icon" href="favicon.png">
+</head>
+
+<body>
+       <div class="headertop container-fluid">
+         <div class="row">
+           <div class="col-lg-3">
+             <img class="accueil-logo "src="logo.png" alt="logo">
+             <img class="accueil-logo2 "src="logo-mobile.png" alt="logo">
+           </div>
+               <div class="col-lg-3">
+                 <img class="accueil-loupe" src="loupe.png" alt="loupe">
+                 <div class="accueil-search">
+                   <form action="/action_page.php">
+                     <input class="accueil-recherche" type="text" placeholder="Rechercher votre sticker par région..." name="search">
+                   </form>
+                 </div>
+               </div>
+                <div class="accueil-icones col-lg-6 text-right">
+                   <a class="accueil-tel" href="#"><img class="atel" src="telephone.png" alt="icontelephone"></a>
+                   <a class="accueil-user" href="#"><img class="auser" src="user.png" alt="compte"></a>
+                   <a class="accueil-panier" href="#"><img class="apanier" src="panier.png" alt="panier"></a>
+                   <a class="accueil-panier2" href="#"><img class="apanier2" src="panier-mobile.png" alt="panier"></a>
+                   <a class="accueil-burger" href="#"><img class="accueil-burger2" src="burger.png" alt="panier"></a>
+                 </div>
+                   </nav>
+                  </a>
+                </div>
+              </div>
+
+            <div class="accueil-part1 container-fluid">
+              <div class="row">
+                <div class="col-lg-12">
+                <div class="accueil-slo">
+                  <p class="accueil-slogan1">Affichez son attachement</p>
+                  <p class="accueil-slogan2"> à son département et ses valeurs...</p>
+                  <p class="mobile-slogan1">Affichez son attachement</p>
+                  <p class="mobile-slogan2"> à son département et ses valeurs...</p>
+                </div>
+                <div class="accueil-car">
+                  <img class="accueil-voiture" src="img/avoiture.jpg" alt="voiture">
+                  <img class="accueil-voiture2" src="voiture-mobile.png" alt="voiture">
+                </div>
+                <div class="accueil-voiture2">
+        <a href="https://www.sticker-autocollant-plaque.com/15-collection-1" target="_blank"><button type="button" class="accueil-btn btn-outline-primary">Voir nos produits</button></a>
+        <a href="https://www.sticker-autocollant-plaque.com/15-collection-1" target="_blank"><button type="button" class="accueil-btn2 btn-outline-primary">Voir nos produits</button></a>
+                 </div>
+               </div>
+             </div>
+           </div>
+                      <!--navbar-->
+           <ul class="accueil-menu container-fluid">
+             <li class="accueil-menu2"><a class="accueil-liste" href="#Accueil">Accueil</a></li>
+             <li class="accueil-menu2"><a class="accueil-liste2" href="#Collections">Nos collections</a></li>
+             <li class="accueil-menu2"><a class="accueil-liste3" href="#Qui">Qui sommes nous</a></li>
+             <li class="accueil-menu2"><a class="accueil-liste4" href="#Contact">Nous contacter</a></li>
+           </ul>
+
+
+
+
+           <div class="rectangle-noir">
+           </div>
+           <div class="collections container-fluid">
+             <div class="row">
+             <div class="accueil-rectangle1">
+               <p><a class="accueil-collection" href="#Collection1">Collection 1</a></p>
+               <p><a class="accueil-collection2" href="#Collection2">Collection 2</a></p>
+               <p><a class="accueil-collection3" href="#Collection3">Collection 3</a></p>
+               <p class="region">Le sticker région<p>
+               <img class="accueil-car2" src="voiture2.jpg">
+               <img class="resine1" src="resine.png" alt="sticker resine">
+               <a href="https://www.sticker-autocollant-plaque.com/15-collection-1" target="_blank"><button type="button" class="accueil-bouton btn-outline-primary">Voir nos produits</button></a>
+               <a href="#carousel"><img class="fleche1" src="fleche1.png"></a>
+               <a href="#carousel"><img class="fleche2" src="fleche2.png"></a>
+             </div>
+            </div>
+           </div>
+
+               <div class="rectangle2">
+               <p class="accueil-ventes">Meilleures ventes</p>
+
+             <div class="container">
+               <img src="sticker2.png" alt="sticker2" class="image">            <!--2e sticker partie Meilleures ventes-->
+               <div class="overlay">
+                 <div class="text">Stickers ALPES-MARITIMES</div>
+                 <div class="text2">6€<br></div>
+                 <div class="text3">VOIR LE PRODUIT</div>
+               </div>
+             </div>
+
+             <div class="container2">
+               <img src="sticker1.png" alt="sticker1" class="image2">           <!--1er sticker partie Meilleures ventes-->
+               <div class="overlay2">
+                 <div class="text4">Stickers NORD</div>
+                 <div class="text5">6€</div>
+                 <div class="text6">VOIR LE PRODUIT</div>
+               </div>
+             </div>
+
+             <div class="container3">
+               <img src="sticker1.png" alt="sticker3" class="image3">           <!--3e sticker partie Meilleures ventes-->
+               <div class="overlay3">
+                 <div class="text7">Stickers NORD</div>
+                 <div class="text8">6€</div>
+                 <div class="text9">VOIR LE PRODUIT</div>
+               </div>
+             </div>
+           </div>
+
+
+                <!--partie Nouveautés-->
+
+
+           <div class="rectangle3 container-fluid">                             <!--partie noire nouveautés avec titre et logo-->
+             <div class="row">
+               <div class="anouveaute">
+                 <p class="atitre text-right">Les nouveautés</p>
+                 <img class="logo2" src="logo-nouveautes.png">
+               </div>
+               <div class="container4">                                         <!--1er sticker partie nouveautés-->
+                 <img src="sticker4.png" alt="sticker4" class="image4">
+                 <div class="overlay4">
+                   <div class="txt1">Stickers ARDENNES</div>
+                   <div class="txt2">6€</div>
+                   <div class="txt3">VOIR LE PRODUIT</div>
+                 </div>
+               </div>
+               <div class="container5">                                         <!--2e sticker partie nouveautés-->
+                 <img src="sticker5.png" alt="sticker5" class="image5">
+                 <div class="overlay5">
+                   <div class="txt4">Stickers FRANCE</div>
+                   <div class="txt5">6€</div>
+                   <div class="txt6">VOIR LE PRODUIT</div>
+                 </div>
+               </div>
+               <div class="container6">                                         <!--3e sticker partie nouveautés-->
+                 <img src="sticker6.png" alt="sticker6" class="image6">
+                 <div class="overlay6">
+                   <div class="text7">Stickers PYRÉNÉES ORIENTALES</div>
+                   <div class="text8">6€</div>
+                   <div class="txt9">VOIR LE PRODUIT</div>
+                 </div>
+               </div>
+              </div>
+              </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php' ?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
